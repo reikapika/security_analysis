819c1d080de1115f565470b1fc1cfb30d9ef98b5
shioramen <esther.t0000@gmail.com>
2017-03-06 00:18:40 -0800

    updatd readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Security Analysis Tool
 ### Home Assignment for Junior Software Engineer (Security Team)
+####Thoughts
+- I looked at RepoGuard source code and had no idea how I could integrate that.
+- A few things that I have tried:
+GitHub API - github3.py, PyGithub
+  Using Github api should be the best approach to get the content from the input repo link.
+scrapping contents from the repo link directly (bad approach)
+- If I had more time, I'd like to explore and play around with RepoGuard and Let's be Bad Guys tutorial before doing this assignment.
--- server.py
+++ server.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for, jsonify, redirect, request
-
+from flask import Flask, render_template, url_for, jsonify, redirect, request, jsonify
+import json
+import requests
+import urllib2
+from pprint import pprint
 
 # Setting up the web app using Flask
 app = Flask(__name__)
@@ -11,6 +14,15 @@ def index():
 
     return render_template('index.html')
 
+@app.route('/repo_scan', methods=['GET'])
+def repo_scan():
+    repo_url = request.args.get('repo-url')
+    # print '$$$$$$$$$$', repo_url
+    foo = urllib2.urlopen(repo_url)
+    response = foo.read()
+    # print '*********', pprint(response)
+
+    return jsonify(result=True)
 
 
 if __name__ == "__main__":
